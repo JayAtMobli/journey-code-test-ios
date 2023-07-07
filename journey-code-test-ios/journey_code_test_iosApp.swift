0fe64b275ac9ff06144ee1aeea20607bb1a23095
Jay Park <cocoalovesoft@gmail.com>
2023-07-07 12:04:30 +1200

    initial xcode project created

---
+++ journey-code-test-ios/journey_code_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  journey_code_test_iosApp.swift
+//  journey-code-test-ios
+//
+//  Created by Jay Park on 7/07/23.
+//
+
+import SwiftUI
+
+@main
+struct journey_code_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
