0a6903b37b62882b20654a0927df33351c26cb6c
Jay Park <cocoalovesoft@gmail.com>
2023-07-08 10:04:09 +1200

    View place holders

---
+++ journey-code-test-ios/Views/CommentList/CommentListView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentListView.swift
+//  journey-code-test-ios
+//
+//  Created by Jay Park on 8/07/23.
+//
+
+import SwiftUI
+
+struct CommentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentListView()
+    }
+}
---
+++ journey-code-test-ios/Views/PostList/PostListView.swift
@@ -0,0 +1,20 @@
+//
+//  PostListView.swift
+//  journey-code-test-ios
+//
+//  Created by Jay Park on 8/07/23.
+//
+
+import SwiftUI
+
+struct PostListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostListView()
+    }
+}
