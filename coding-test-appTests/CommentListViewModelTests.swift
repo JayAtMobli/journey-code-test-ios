36d66016b7543cc767a889c872260deb7fa06f18
Jay Park <cocoalovesoft@gmail.com>
2023-07-16 08:53:40 +1200

    more test cases for view models

--- coding-test-appTests/CommentListViewModelTests.swift
+++ coding-test-appTests/CommentListViewModelTests.swift
@@ -8,20 +8,26 @@
 import XCTest
 @testable import coding_test_app
 
+@MainActor
 final class CommentListViewModelTests: XCTestCase {
     private var vm: CommentListViewModel!
     
-    @MainActor override func setUp() {
+    override func setUp() {
         let mockedService = MockedService()
         let post = Post(userId: 1, id: 1)
         vm = CommentListViewModel(selectedPost: post, service: mockedService)
     }
-
-    @MainActor func testViewModelSearchWithSuccess() async {
+    
+    func testViewModelGetCommentsSuccess() async {
         Task {
             await vm.getComments()
             XCTAssertEqual(vm.comments.count, 5)
-            
+        }
+    }
+
+    func testViewModelSearchWithSuccess() async {
+        Task {
+            await vm.getComments()
             vm.searchTerms = "Eliseo@gardner.biz"
             XCTAssertEqual(vm.filteredComments.count, 1)
         }
--- coding-test-appTests/PostListViewModelTests.swift
+++ coding-test-appTests/PostListViewModelTests.swift
@@ -8,21 +8,27 @@
 import XCTest
 @testable import coding_test_app
 
-
+@MainActor
 final class PostListViewModelTests: XCTestCase {
     private var vm: PostListViewModel!
     
-    @MainActor override func setUp() {
+    override func setUp() {
         let mockedService = MockedService()
         let coordinator = HomeCoordinator(service: mockedService)
         vm = PostListViewModel(service: mockedService, coordinator: coordinator)
     }
     
-    @MainActor func testViewModelSearchWithSuccess() async {
+    func testViewModelGetPostsSuccess() async {
         Task {
             await vm.getPosts()
             XCTAssertEqual(vm.posts.count, 100)
-            
+        }
+    }
+    
+    
+    func testViewModelSearchSuccess() async {
+        Task {
+            await vm.getPosts()
             vm.searchTerms = "qui es"
             XCTAssertEqual(vm.filteredPosts.count, 2)
         }
